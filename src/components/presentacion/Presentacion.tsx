d5bee3bf4d3df4ae86d1bdf9d3756cc396a1582d
Frank <fcarpio113@gmail.com>
2025-08-27 20:37:42 -0500

    Add presentation images and update Presentacion component to include imageURL prop

--- src/components/presentacion/Presentacion.tsx
+++ src/components/presentacion/Presentacion.tsx
@@ -7,35 +7,36 @@ interface PresentacionProps {
     subtitle?: string;
     fecha: Fecha;
     hora: string;
+    lugar: string;
     lugarURL?: string;
+    imageURL: string;
 }
 
-const Presentacion: React.FC<PresentacionProps> = ({ title, subtitle, fecha, hora, lugarURL }) => {
+const Presentacion: React.FC<PresentacionProps> = ({ title, subtitle, fecha, hora, lugar, lugarURL, imageURL }) => {
     return (
-        <div className="flex justify-between items-center m-2 px-4 py-4 border-2 rounded shadow transition border-gray-600 bg-white">
-        <div className="flex flex-col">
+        <div className="relative m-2 rounded-xl shadow transform transition bg-white ">
+          <div className="absolute top-2 left-2">
+            <div className="bg-blue-800 rounded-lg px-2 py-1 text-center select-none">
+              <p className="text-white text-lg">{fecha.mes}</p>
+              <p className="text-white text-3xl font-semibold">{fecha.dia}</p>
+            </div>
+          </div>
+            <div className="h-64 overflow-hidden flex items-center justify-center bg-gray-100">
+            <img
+              className="w-full h-full object-cover rounded-t-xl"
+              src={imageURL}
+              alt="imagen de presentacion"
+            />
+            </div>
+        <div className="flex flex-col p-4 pb-2">
           <span className="md:text-3xl font-bold text-gray-900">{title || "Titulo"}</span>
           <span className="sm:text-sm text-md text-gray-600">
             {subtitle || "Subtitulo"}
           </span>
         </div>
-        <div className="flex items-center gap-5">
-          <div className="flex flex-col items-center border-b border-l border-r border-gray-400 rounded text-white font-semibold">
-            <span className="bg-slate-800 rounded-t px-2 py-1 w-full text-center text-sm">{fecha?.mes || "Mes"}</span>
-            <span className="px-2 py-1 bg-slate-400 w-full text-center text-sm">
-            {fecha?.dia || "Día"}
-            </span>
-          </div>
-          <div className="flex flex-col items-center border-b border-l border-r border-gray-400 rounded text-white font-semibold">
-            <span className="bg-slate-800 rounded-t px-2 py-1 w-full text-center text-sm">Hora</span>
-            <span className="px-2 py-1 bg-slate-400 w-full text-center text-sm">
-            {hora || "Hora"}
-            </span>
-          </div>
-          <div>
-            <a href={lugarURL} target="_blank">
-                <i className="fa-solid fa-map-location-dot text-3xl text-gray-700"></i>
-            </a>
+        <div className="flex gap-4 px-4 py-2 pb-4">
+          <div className="text-gray-500 font-semibold text-lg">
+              {hora} | <a href={lugarURL} target="_blank" rel="noopener noreferrer">{lugar}</a>
           </div>
         </div>
       </div>
--- src/screens/Eventos/Eventos.tsx
+++ src/screens/Eventos/Eventos.tsx
@@ -2,35 +2,44 @@ import BtnScroll from "../../components/btn-scroll/BtnScroll";
 import Footer from "../../components/footer/Footer";
 import Navbar from "../../components/nav-bar/Navbar";
 import Presentacion from "../../components/presentacion/Presentacion";
+import Titulo from "../../components/Titulo/Titulo";
 
 function Eventos() {
   return (
-    <main className="bg-gray-100">
+    <main className="bg-gray-100 ">
       <BtnScroll />
       <Navbar />
-      <div className="text-4xl font-bold text-center my-4">Presentaciones</div>
-      <div className="my-7 mx-4 space-y-6">
-        <Presentacion
-          title="Libre Soy"
-          subtitle="presentación de coreografía"
-          fecha={{ mes: "MAR", dia: "02" }}
-          hora="6:00 PM"
-          lugarURL="https://maps.app.goo.gl/wELUNuhSepefKZWs9"
-        />
-        <Presentacion
-          title="Mix Bueno Es Dios | Mix Rey Vencedor"
-          subtitle="presentación de coreografía"
-          fecha={{ mes: "MAR", dia: "09" }}
-          hora="6:00 PM"
-          lugarURL="https://maps.app.goo.gl/wELUNuhSepefKZWs9"
-        />
-        <Presentacion
-          title="Lo Mio No Pasa"
-          subtitle="presentación de coreografía"
-          fecha={{ mes: "MAR", dia: "16" }}
-          hora="6:00 PM"
-          lugarURL="https://maps.app.goo.gl/wELUNuhSepefKZWs9"
-        />
+      <div className="container mx-auto">
+        <Titulo type="h1" mensaje="Eventos" className="mt-28"></Titulo>
+        <div className="my-7 mx-4 grid grid-cols-1 sm:grid-cols-2 gap-4">
+          <Presentacion
+            title="Libre Soy"
+            subtitle="presentación de coreografía"
+            fecha={{ mes: "Marzo", dia: "02" }}
+            hora="6:00 PM"
+            lugar="Iglesia"
+            lugarURL="https://maps.app.goo.gl/wELUNuhSepefKZWs9"
+            imageURL="presentacion-image.jpg"
+          />
+          <Presentacion
+            title="Mix Bueno Es Dios | Mix Rey Vencedor"
+            subtitle="presentación de coreografía"
+            fecha={{ mes: "MAR", dia: "09" }}
+            hora="6:00 PM"
+            lugar="Iglesia"
+            lugarURL="https://maps.app.goo.gl/wELUNuhSepefKZWs9"
+            imageURL="presentacion-image2.jpg"
+          />
+          <Presentacion
+            title="Lo Mio No Pasa"
+            subtitle="presentación de coreografía"
+            fecha={{ mes: "MAR", dia: "16" }}
+            hora="6:00 PM"
+            lugar="Iglesia"
+            lugarURL="https://maps.app.goo.gl/wELUNuhSepefKZWs9"
+            imageURL="presentacion-image3.jpg"
+          />
+        </div>
       </div>
 
       <Footer />
